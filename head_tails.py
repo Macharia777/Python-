228301921a166b9e5087a1a2efbeb15e39e2ecd2
Macharia777 <gacheruarchibald@gmail.com>
2023-06-05 13:29:20 +0300

    random

--- head_tails.py
+++ head_tails.py
@@ -1,7 +1,15 @@
+# import random
+
+# random_number = random.randint(0,1)
+# if (random_number == 0):
+#     print("heads")
+# else:
+#     print("tails")
+
 import random
 
-random_number = random.randint(0,1)
-if (random_number == 0):
+random_number = random.randint(0, 1)
+if random_number == 1:
     print("heads")
 else:
     print("tails")
--- mathexecptions.py
+++ mathexecptions.py
@@ -43,46 +43,73 @@
 #         print("You guessed it")
 #         break
 
-import math
+# import math
 
-# Because you used import you can access methods by referencing the module
-print("ceil(4.4) = ", math.ceil(4.4))
-print("floor(4.4) = ", math.floor(4.4))
-print("fabs(-4.4) = ", math.fabs(-4.4))
+# # Because you used import you can access methods by referencing the module
+# print("ceil(4.4) = ", math.ceil(4.4))
+# print("floor(4.4) = ", math.floor(4.4))
+# print("fabs(-4.4) = ", math.fabs(-4.4))
 
-# Factorial = 1 * 2 * 3 * 4
-print("factorial(4) = ", math.factorial(4))
+# # Factorial = 1 * 2 * 3 * 4
+# print("factorial(4) = ", math.factorial(4))
 
-# return remainder or division
-print("fmod(6,4) = ", math.fmod(6, 4))
+# # return remainder or division
+# print("fmod(6,4) = ", math.fmod(6, 4))
 
-# Receive a float and return and int
-print("trunc(4.2) = ", math.trunc(4.2))
+# # Receive a float and return and int
+# print("trunc(4.2) = ", math.trunc(4.2))
 
-# Return x^y
-print("pow(2,2) = ", math.pow(2, 2))
+# # Return x^y
+# print("pow(2,2) = ", math.pow(2, 2))
 
-# Return the square root
-print("sqrt(4) = ", math.sqrt(4))
+# # Return the square root
+# print("sqrt(4) = ", math.sqrt(4))
 
-# Special Values
-print("math.e = ", math.e)
-print("math.pi = ", math.pi)
+# # Special Values
+# print("math.e = ", math.e)
+# print("math.pi = ", math.pi)
 
-# Return e^x
-print("exp(4) = ", math.factorial(4))
+# # Return e^x
+# print("exp(4) = ", math.factorial(4))
 
-# Return the natural logarithm e * e * e ~= 20 so log(20) tells
-# you that e^3 ~= 20
-print("log(1000,10) = ", math.log(1000, 10))
+# # Return the natural logarithm e * e * e ~= 20 so log(20) tells
+# # you that e^3 ~= 20
+# print("log(1000,10) = ", math.log(1000, 10))
 
-# You can also use base 10 like this
-print("log10(1000) = ", math.log10(1000))
+# # You can also use base 10 like this
+# print("log10(1000) = ", math.log10(1000))
 
-# we have the following trig functions
-# sin cos tan asin, acos, atan, atanZ, asinh, acosh,
-# atanh, sinh, cosh, tanh
+# # we have the following trig functions
+# # sin cos tan asin, acos, atan, atanZ, asinh, acosh,
+# # atanh, sinh, cosh, tanh
 
-# convert radians to degrees and vice versa
-print("degrees(1.5708) = ", math.degrees(1.5708))
-print("radians(90) = ", math.radians(90))
+# # convert radians to degrees and vice versa
+# print("degrees(1.5708) = ", math.degrees(1.5708))
+# print("radians(90) = ", math.radians(90))
+
+# from decimal import Decimal as D  # This is to make D as an alias for decimal numbers
+
+# # this will change an integer to a float
+
+# sum = D(0)
+# sum += D("0.1")
+# sum += D("0.1")
+# sum += D("0.1")
+# sum -= D("0.3")
+
+# print("sum =", sum)
+
+# print(".1 + .1 + .1 - .3", 0.1 + 0.1 + 0.1 - 0.3)
+
+# We use type to check data types
+# a = type(3)
+# b = type("This is a boy")
+# c = type("hey boy")
+# d = type(3.22)
+# print(a, b, c, d)
+
+# Let's check the first letter of the string
+samp_string = "This is a very important string"
+
+print(samp_string[0])  # This will print out 'T'
+
